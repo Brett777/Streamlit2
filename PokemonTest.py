ad441da2f86dfbf265a3803fac708fff272a27ca
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 20:30:30 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -4,14 +4,11 @@ from forex_python.converter import CurrencyRates
 
 pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 
+cardName = st.text_input("Card Name", "Guzzlord")
+cardNumber = st.text_input("Card Number", "116")
+myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
+st.image(myCard[0].images.small)
+st.write("30 Day Average  $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
+st.write("Alltime Average $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
+st.write(str(myCard[0].rarity))
 
-def getPrice():
-    cardName = st.text_input("Card Name", "Guzzlord")
-    cardNumber = st.text_input("Card Number", "116")
-    myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
-    st.image(myCard[0].images.small)
-    st.write("30 Day Average  $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
-    st.write("Alltime Average $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
-    st.write(str(myCard[0].rarity))
-
-st.button(label="Get Price", on_click=getPrice())
