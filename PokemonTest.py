4e2d51f008a520cf13de0f569c7b52edc24ece8c
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 19:12:14 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -1,12 +1,11 @@
 import pokemontcgsdk as pk
 import streamlit as st
-
 pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 
-myCard = pk.Card.where(q='name:Guzzlord number:116')
+cardName = st.text_input("Card Name", "Guzzlord")
+cardNumber = st.text_input("Card Number", "116")
 
-
-myCard[0].cardmarket.prices.averageSellPrice
+myCard = pk.Card.where(q='name:' + cardName + 'number:' + cardNumber)
 
 
 st.write("This is a Pokemon Card")
