42049ef73cec4faaabcf58e1ce7657be23062283
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 19:30:38 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -1,11 +1,16 @@
 import pokemontcgsdk as pk
 import streamlit as st
+from forex_python.converter import CurrencyRates
+
 pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 
 cardName = st.text_input("Card Name", "Guzzlord")
 cardNumber = st.text_input("Card Number", "116")
 
 myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
+c = CurrencyRates()
+exchangeRate = c.get_rate('EUR', 'CAD')
+
 
 st.write("This is a Pokemon Card")
-st.write("It is worth: " +str(myCard[0].cardmarket.prices.averageSellPrice))
+st.write("It is worth: " +str(int(myCard[0].cardmarket.prices.averageSellPrice)*exchangeRate))
