b7f8a42625e0fc3bb06455531c96bf4b2aa08958
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 19:15:10 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -5,8 +5,7 @@ pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 cardName = st.text_input("Card Name", "Guzzlord")
 cardNumber = st.text_input("Card Number", "116")
 
-myCard = pk.Card.where(q='name:' + cardName + 'number:' + cardNumber)
-
+myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
 
 st.write("This is a Pokemon Card")
 st.write("It is worth: " +str(myCard[0].cardmarket.prices.averageSellPrice))
