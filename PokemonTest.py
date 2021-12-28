eebf468b0391af785e19de4f07c84a3e4e9abf5b
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 20:33:41 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -10,7 +10,7 @@ try:
     myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
     st.image(myCard[0].images.small)
     st.write("30 Day Average  $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
-    st.write("Alltime Average $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
+    st.write("Alltime Average $" +str(round(int(myCard[0].cardmarket.prices.averageSellPrice)*1.4502,2)))
     st.write(str(myCard[0].rarity))
 except:
     print("Enter a card name and number.")
