a6869074ab123931e4a49cb00403d8e284d5a281
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-27 20:26:17 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -7,10 +7,11 @@ pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 cardName = st.text_input("Card Name", "Guzzlord")
 cardNumber = st.text_input("Card Number", "116")
 
-myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
-#c = CurrencyRates()
-#exchangeRate = c.get_rate('EUR', 'CAD')
+def getPrice(cardName,cardNumber):
+    myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
+    st.image(myCard[0].images.small)
+    st.write("30 Day Average  $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
+    st.write("Alltime Average $" +str(round(int(myCard[0].cardmarket.prices.avg30)*1.4502,2)))
+    st.write(str(myCard[0].rarity))
 
-st.image(myCard[0].images.small)
-
-st.write("$" +str(int(myCard[0].cardmarket.prices.averageSellPrice)*1.4502))
+st.button(label="Get Price", on_click=getPrice(cardName,cardNumber))
