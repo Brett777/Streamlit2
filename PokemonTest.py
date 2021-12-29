05ebed28247b128011e8586298e1e58b3acaa53f
Brett Olmstead <scruffyruskin@gmail.com>
2021-12-28 20:45:02 -0500

    Add files via upload

--- PokemonTest.py
+++ PokemonTest.py
@@ -4,7 +4,7 @@ from forex_python.converter import CurrencyRates
 
 pk.RestClient.configure('21b4e399-5973-4bf5-b9be-009df2d6b396')
 
-st.title("Pokemon Card Value")
+st.title("Pokemon Card Checker")
 st.header("Enter a card name and number to get its market value and rarity.")
 
 try:
@@ -15,6 +15,7 @@ try:
 
     if submit_button:
         myCard = pk.Card.where(q='name:' + str(cardName) + ' number:' + str(cardNumber))
+        myCard = pk.Card.where(q='name:' + str(cardName))
         st.image(myCard[0].images.large)
         avg30 = round(myCard[0].cardmarket.prices.avg30*1.4502,2)
         avgAll = round(myCard[0].cardmarket.prices.averageSellPrice * 1.4502, 2)
